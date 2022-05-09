b86f0ac6610ad7d2d0bb6adf6a3a30cbd31b0787
Avinash Anantharamu <avinashanantharamu@outlook.com>
2022-05-08 20:07:35 -0700

    Create pytube_streamlit.py

---
+++ pytube_streamlit.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+from pytube import YouTube
+import base64
+from io import BytesIO
+def main():
+	path = st.text_input('Enter URL of any youtube video')
+	option = st.selectbox(
+     'Select type of download',
+     ('audio', 'highest_resolution', 'lowest_resolution'))
+	
+	matches = ['audio', 'highest_resolution', 'lowest_resolution']
+	if st.button("download"): 
+		video_object =  YouTube(path)
+		st.write("Title of Video: " + str(video_object.title))
+		st.write("Number of Views: " + str(video_object.views))
+		if option=='audio':
+			video_object.streams.get_audio_only().download() 		#base64.b64encode(csv.encode()).decode()	
+		elif option=='highest_resolution':
+			video_object.streams.get_highest_resolution().download()
+		elif option=='lowest_resolution':
+			video_object.streams.get_lowest_resolution().download()
+	if st.button("view"): 
+		st.video(path) 
+if __name__ == '__main__':
+	main()
+	

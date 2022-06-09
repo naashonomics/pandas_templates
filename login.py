9e124befdc8ebae07764d1397ea92f35c2265268
Avinash Anantharamu <avinashanantharamu@outlook.com>
2022-06-08 22:45:18 -0700

    Update login.py

--- login.py
+++ login.py
@@ -1,24 +1,25 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 
+def login():
+    names = ['Join Prime','Join Walmart plus']
+    usernames = ['amazon','walmart']
+    passwords = ['amazonpay','phonepe']
+    hashed_passwords = stauth.Hasher(passwords).generate()
+    authenticator = stauth.Authenticate(names,usernames,hashed_passwords,'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+    name, authentication_status, username = authenticator.login('Login', 'main')
+        
+    if st.session_state["authentication_status"]:
+        test=authenticator.logout('Logout', 'main')
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        st.title('Some content')
+    elif st.session_state["authentication_status"] == False:
+        st.error('Username/password is incorrect')
+    elif st.session_state["authentication_status"] == None:
+        st.warning('Please enter your username and password')
+        
+def main():
+    login()
 
-names = ['Join Prime','Join Walmart plus']
-usernames = ['amazon','walmart']
-passwords = ['amazonpay','phonepe']
-
-hashed_passwords = stauth.Hasher(passwords).generate()
-
-authenticator = stauth.Authenticate(names,usernames,hashed_passwords,'some_cookie_name','some_signature_key',cookie_expiry_days=30)
-	
-	
-name, authentication_status, username = authenticator.login('Login', 'main')
-
-	
-if st.session_state["authentication_status"]:
-    test=authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-elif st.session_state["authentication_status"] == False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] == None:
-    st.warning('Please enter your username and password')
+if __name__ == "__main__":
+    main()

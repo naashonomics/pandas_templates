c5d46881709b95b8a33587b8d771de5006ac8807
Avinash Anantharamu <naashonomics@gmail.com>
2023-01-27 21:09:29 -0800

    Add files via upload

---
+++ demo_datatable.py
@@ -0,0 +1,36 @@
+import webbrowser
+import pandas as pd
+
+def generate_html(dataframe: pd.DataFrame):
+    # get the table HTML from the dataframe
+    table_html = dataframe.to_html(table_id="table")
+    # construct the complete HTML with jQuery Data tables
+    # You can disable paging or enable y scrolling on lines 20 and 21 respectively
+    html = f"""
+    <html>
+    <header>
+        <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
+    </header>
+    <body>
+    {table_html}
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready( function () {{
+            $('#table').DataTable({{
+                // paging: false,    
+                // scrollY: 400,
+            }});
+        }});
+    </script>
+    </body>
+    </html>
+    """
+    # return the html
+    return html
+
+if __name__ == "__main__":
+	df = pd.read_excel('Financial_Sample.xlsx',sheet_name='Sheet1')
+	html = generate_html(df)
+	open("index.html", "w").write(html)
+	webbrowser.open("index.html")

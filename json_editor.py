77fbb6857ba4aff346f1d5b01916c9450ca0adec
Avinash Anantharamu <naashonomics@gmail.com>
2023-05-18 14:52:27 -0700

    Add files via upload

---
+++ json_editor.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import json
+
+# Function to load JSON
+def load_json(uploaded_file):
+    data = json.load(uploaded_file)
+    return data
+
+# Function to save JSON
+def save_json(file, data):
+    with open(file, 'w') as f:
+        json.dump(data, f, indent=4)
+
+# Upload the JSON file
+uploaded_file = st.file_uploader("Choose a JSON file", type='json')
+if uploaded_file is not None:
+    data = load_json(uploaded_file)
+
+    # Display JSON in a text area (as a string for editing)
+    json_str = st.text_area("JSON Input", json.dumps(data, indent=4))
+    edited_data = json.loads(json_str)
+
+    # Input for the file path
+    file_path = st.text_input("Enter the file path where you want to save the edited file")
+
+    # Button to save the changes
+    if st.button('Save JSON'):
+        if file_path:
+            save_json(file_path.strip(), edited_data)
+            st.success('File saved')
+        else:
+            st.error('Please enter a file path')
